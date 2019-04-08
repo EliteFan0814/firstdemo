fd2a3c5489afe987bd8d9225faf4a882eb9b67f8
EliteFan <fan552426811@outlook.com>
2019-04-08 21:31:08 +0800

    this

--- js/message.js
+++ js/message.js
@@ -1,48 +1,70 @@
-var APP_ID = 'vWPMDb3tWUd6t7jPBlxyXsHw-gzGzoHsz';
-var APP_KEY = 'Hti6mUX9b8p1MWcNdvC1pWE4';
+!function () {
+  var view = document.querySelector('section.messageBoard')
 
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
+  var controller = {
+    view: null,
+    messageList: null,
+    myMesssageForm: null,
+    init: function (view) {
+      this.view = view,
+        this.messageList = view.querySelector('#messageList'),
+        this.myMesssageForm = view.querySelector('#myMesssageForm'),
+        this.initAV(),
+        this.loadMessage(),
+        this.bindEvents()
+    },
+    initAV: function () {
+      var APP_ID = 'vWPMDb3tWUd6t7jPBlxyXsHw-gzGzoHsz'
+      var APP_KEY = 'Hti6mUX9b8p1MWcNdvC1pWE4'
 
-let query = new AV.Query('formMessage')
-let ul = document.querySelector('#messageList')
+      AV.init({ appId: APP_ID, appKey: APP_KEY })
+    },
+    loadMessage: function () {
+      let query = new AV.Query('formMessage')
 
-query.find().then(function (message) {
-  console.log(message)
-  let array = message.map((items) => { return items.attributes })
-  console.log(array)
-  array.forEach((element) => {
-    let li = document.createElement('li')
-    li.innerHTML = element.name + ' ：' + element.content
-    ul.append(li)
-  })
-}, function () { })
-
-let myMesssageForm = document.querySelector('#myMesssageForm')
-
-myMesssageForm.addEventListener('submit', function (e) {
-  e.preventDefault()
-  let name = myMesssageForm.querySelector('[name=name]').value
-  let content = myMesssageForm.querySelector('[name=content]').value
-  if (name && content) {
-    var FormMessage = AV.Object.extend('formMessage');
-    var formMessage = new FormMessage();
-    formMessage.save({
-      name: name,
-      content: content
-    }).then(function (object) {
-      console.log(object)
-      let li = document.createElement('li')
-      li.innerHTML = object.attributes.name + '说：' + object.attributes.content
-      ul.append(li)
-      myMesssageForm.querySelector('[name=name]').value = ''
-      myMesssageForm.querySelector('[name=content]').value = ''
-    })
-  } else {
-    alert('内容不得为空！')
+      query.find().then((message) => {
+        let array = message.map((items) => { return items.attributes })
+        array.forEach((element) => {
+          let li = document.createElement('li')
+          li.innerHTML = element.name + ' ：' + element.content
+          this.messageList.append(li)
+        })
+      }, () => { error })
+    },
+    bindEvents: function () {
+      this.myMesssageForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        // console.log(this)
+        this.writeMessage()
+      })
+    },
+    writeMessage: function () {
+      let myMesssageForm = this.myMesssageForm
+      let name = myMesssageForm.querySelector('[name=name]').value
+      let content = myMesssageForm.querySelector('[name=content]').value
+      if (name && content) {
+        var FormMessage = AV.Object.extend('formMessage');
+        var formMessage = new FormMessage();
+        formMessage.save({
+          name: name,
+          content: content
+        }).then(function (object) {
+          console.log(object)
+          let li = document.createElement('li')
+          li.innerHTML = object.attributes.name + '说：' + object.attributes.content
+          messageList.append(li)
+          myMesssageForm.querySelector('[name=name]').value = ''
+          myMesssageForm.querySelector('[name=content]').value = ''
+        })
+      } else {
+        alert('内容不得为空！')
+      }
+    }
   }
 
-})
+  controller.init(view)
+
+}.call()
+
+
 
